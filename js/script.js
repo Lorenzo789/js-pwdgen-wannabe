30b66e92c89171745b6f51a1595ed7fde262749b
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-06-13 16:21:36 +0200

    comments

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
+
+// Name //
 const userName = prompt('inserisci il tuo nome');
 document.getElementById('user-name').innerHTML = userName;
 
+// Surname //
 const userSurName = prompt('inserisci il tuo cognome');
 document.getElementById('user-surname').innerHTML = userSurName;
 
+// Favorite Color //
 const userFavoriteColor = prompt('inserisci il tuo colore preferito');
 document.getElementById('user-fav-color').innerHTML = userFavoriteColor;
 
+
+// Name + Surname + Favorite Color //
 let userInfo = userName + userSurName + userFavoriteColor + 22;
 document.getElementById('user-info').innerHTML = userInfo
